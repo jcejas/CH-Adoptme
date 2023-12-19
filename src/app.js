0cc6b8042353003fe1881f6b5c761628c6f75b92
Joaquin Cejas <joaquin.cejas.2020@gmail.com>
2023-12-18 22:29:47 -0300

    Add home page

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import handlebars from 'express-handlebars';
 
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
+import viewsRouter from './routes/views.router.js';
+import __dirname from './utils/index.js';
 
 const app = express();
 const PORT = process.env.PORT||8080;
@@ -14,9 +17,15 @@ const connection = mongoose.connect(process.env.MONGO_URL);
 app.use(express.json());
 app.use(cookieParser());
 
+//Handlebars Config
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/../views');
+app.set('view engine', 'handlebars');
+
 app.use('/api/users',usersRouter);
 app.use('/api/pets',petsRouter);
 app.use('/api/adoptions',adoptionsRouter);
 app.use('/api/sessions',sessionsRouter);
+app.use('/',viewsRouter);
 
 app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
